cf7c282a91643167f055cefc3754ea050f61b6dc
parthgupta9 <parth615gupta@gmail.com>
2024-09-01 01:01:13 +0530

    database setup

--- server/config/database.js
+++ server/config/database.js
@@ -1,12 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const databaseConnection = () => {
-    mongoose.connect(process.env.DB,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then((data)=>{
-        console.log(`database connected successfully at server ${data.connection.host}`)
-    })
-}
+    mongoose.connect(
+        process.env.DB || 'mongodb+srv://parth:qzit5532@projectsnodeexpress.nfogjjc.mongodb.net/?retryWrites=true&w=majority&appName=ProjectsNodeExpress',
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    ).then((data) => {
+        console.log(`Database connected successfully at server ${data.connection.host}`);
+    }).catch((error) => {
+        console.error('Database connection error:', error);
+        process.exit(1); // Optional: exit process if the connection fails
+    });
+};
 
-module.exports = databaseConnection
+module.exports = databaseConnection;
